549dd17e4d4d3e7dccde0da2f997bdaedc247a45
MohdJunaid <junaid03mo22@gmail.com>
2025-12-18 00:11:59 +0530

    done with core content section

---
+++ landing.js
@@ -0,0 +1,29 @@
+const dropElement1 = document.getElementById("dropdown1");
+const dropElement2 = document.getElementById("dropdown2");
+const dropElement3 = document.getElementById("dropdown3");
+
+
+function togglechange1(){
+    if (dropElement1.style.display==="none"){
+        dropElement1.style.display="block";
+
+    }else{
+        dropElement1.style.display='none';
+    }
+}
+function togglechange2(){
+    if (dropElement2.style.display==="none"){
+        dropElement2.style.display="block";
+
+    }else{
+        dropElement2.style.display='none';
+    }
+}
+function togglechange3(){
+    if (dropElement3.style.display==="none"){
+        dropElement3.style.display="block";
+
+    }else{
+        dropElement3.style.display='none';
+    }
+}
