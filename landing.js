e3c17de4e80c49cd8d59b4872198d9fc15fedc24
MohdJunaid <junaid03mo22@gmail.com>
2025-12-19 17:55:52 +0530

    done with content section

--- landing.js
+++ landing.js
@@ -1,6 +1,9 @@
 const dropElement1 = document.getElementById("dropdown1");
 const dropElement2 = document.getElementById("dropdown2");
 const dropElement3 = document.getElementById("dropdown3");
+const dropElement4 = document.getElementById("dropdown4");
+const dropElement5 = document.getElementById("dropdown5");
+const dropElement6 = document.getElementById("dropdown6");
 
 
 function togglechange1(){
@@ -25,5 +28,29 @@ function togglechange3(){
 
     }else{
         dropElement3.style.display='none';
+    }
+}
+function togglechange4(){
+    if (dropElement4.style.display==="none"){
+        dropElement4.style.display="block";
+
+    }else{
+        dropElement4.style.display='none';
+    }
+}
+function togglechange5(){
+    if (dropElement5.style.display==="none"){
+        dropElement5.style.display="block";
+
+    }else{
+        dropElement5.style.display='none';
+    }
+}
+function togglechange6(){
+    if (dropElement6.style.display==="none"){
+        dropElement6.style.display="block";
+
+    }else{
+        dropElement6.style.display='none';
     }
 }
